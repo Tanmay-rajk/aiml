f74172a453d8c9f65c1ffc885c4e6802dfe8fabf
Tanmay R K <111168823+Tanmay-rajk@users.noreply.github.com>
2024-12-14 10:22:58 +0530

    Update program4.py

--- program4.py
+++ program4.py
@@ -1,3 +1,5 @@
+# Bfs part A
+
 from collections import deque
 def bfs(graph, start):
 visited = set()
@@ -24,3 +26,29 @@ graph = {
 print("bfs starting from A")
 start_node = 'A'
 paths = bfs(graph, start_node)
+
+#Part B
+
+def dfs(graph, node, visited=None, path=None):
+if visited is None:
+visited = set()
+if path is None:
+path = []
+if node not in visited:
+visited.add(node)
+path.append(node)
+print(f"visited: {node}, path: {path}")
+for neighbor in graph[node]:
+if neighbor not in visited:
+dfs(graph, neighbor, visited, path)
+graph = {
+'A': ['B', 'C'],
+'B': ['A', 'D', 'E'],
+'C': ['A', 'F'],
+'D': ['B'],
+'E': ['B', 'F'],
+'F': ['C', 'E']
+}
+print("dfs starting from A")
+start_node = 'A'
+dfs(graph, start_node)
